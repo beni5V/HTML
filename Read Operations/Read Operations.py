4276c301a5d1ad436ab9c4b426ceb9b0849c19c7
beni5V <bushrabukhari72@gmail.com>
2026-07-16 20:08:57 +0500

    intial commit

---
+++ Read Operations/Read Operations.py
@@ -0,0 +1,15 @@
+file_read = open("Condingal.txt","r")
+print("File in read mode-")
+print(file_read.read())
+file_read.close()
+
+file_write = open("Condingal.txt","w")
+
+file_write.write("File in write mode ..........")
+file_write.write("Hi! I am Penguin. I am 1 yr. old")
+file_write.close()
+
+file_append = open("Condngal.txt", "a")
+file_append.write("\n File in append")
+file_append.write("Hi! I am Penguin. I am 1 yr. old")
+file_append.close()
