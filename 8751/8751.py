ba79b72016ba720e149e47989fad58040291eaf2
beni5V <bushrabukhari72@gmail.com>
2026-07-21 19:38:42 +0500

    intial commit

--- 8751/8751.py
+++ 8751/8751.py
@@ -1,8 +1,8 @@
-with open('Codingal.txt', 'w') as file:
+with open('Codingal .txt', 'w') as file:
   file.write("Hi! I am Penguin and I am 1 yr old.")
 file.close()
 
-with open('Codingal.txt', 'r') as file:
+with open('Codingal .txt', 'r') as file:
   data = file.readlines()
   print("Words in this file are....")
   for line in data:
