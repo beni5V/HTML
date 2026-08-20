ab22c2911f0c1ae5ba8cc92f875f62c5b42a1d8d
beni5V <bushrabukhari72@gmail.com>
2026-08-20 20:27:55 +0500

    intial commit

---
+++ Rolling Dice/script.js
@@ -0,0 +1,23 @@
+
+var dices = ['&#9856;', '&#9857;', '&#9858;', '&#9859;', '&#9860;', '&#9861;'];
+var stopped = true;
+
+function change() {
+    var random = Math.floor(Math.random() * 6);
+    dices.innerHTML = dices[random];
+}
+
+function stopStart () {
+    if (stopped) {
+        stopped = false;
+        t = setInterval(change, 100);
+    } else {
+        clearInterval(t);
+        stopped = true;
+    }
+}
+
+window.onload = function() {
+    dice = document.getElementById('dice');
+    stopStart();
+}
